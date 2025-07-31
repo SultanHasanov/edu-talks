dff7315012dd5c37b5c7fc2965a041c1792c4152
sultan <sultanhasanov987@gmail.com>
2025-07-31 15:10:28 +0300

    Убрал прокси для прода

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,40 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/login": {
-        target: "http://85.143.175.100:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/logout": {
-        target: "http://85.143.175.100:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/api": {
-        target: "http://85.143.175.100:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/news": {
-        target: "http://85.143.175.100:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/register": {
-        target: "http://85.143.175.100:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/resend-verification": {
-        target: "http://85.143.175.100:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/login": {
+  //       target: "http://85.143.175.100:8080",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     "/logout": {
+  //       target: "http://85.143.175.100:8080",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     "/api": {
+  //       target: "http://85.143.175.100:8080",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     "/news": {
+  //       target: "http://85.143.175.100:8080",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     "/register": {
+  //       target: "http://85.143.175.100:8080",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     "/resend-verification": {
+  //       target: "http://85.143.175.100:8080",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
